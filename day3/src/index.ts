37ff1e18d42bf1fc5a1d82ca218d027c19ea293e
Stephen Ingram <frowey@gmail.com>
2021-12-06 15:34:32 -0500

    day3 part 2

--- day3/src/index.ts
+++ day3/src/index.ts
@@ -1,54 +1,81 @@
 import { readFile } from "fs/promises";
 
-const raw_input = await readFile(process.argv[2], "utf8");
+type BinaryNumber = 0 | 1;
+type BinaryString = BinaryNumber[];
 
-type Direction = "forward" | "down" | "up";
-type Command = { direction: Direction; steps: number };
-type Location = { position: number; depth: number };
-type AimedLocation = { position: number; depth: number; aim: number };
-
-function parse_command(line: string): Command {
-  const [direction_string, steps] = line.split(" ");
-  const direction: Direction =
-    direction_string === "forward"
-      ? "forward"
-      : direction_string === "down"
-      ? "down"
-      : "up";
-  return { direction, steps: parseInt(steps) };
+function make_binary(line: string): BinaryString {
+  return line
+    .trim()
+    .split("")
+    .map((char) => {
+      if (char === "0") {
+        return 0;
+      }
+      return 1;
+    });
 }
 
-const parsed_input = raw_input.split("\n").map(parse_command);
-const step_1_reducer = (acc: Location, cur: Command) => {
-  if (cur.direction === "down") {
-    acc.depth += cur.steps;
-  }
-  if (cur.direction === "up") {
-    acc.depth -= cur.steps;
-  }
-  if (cur.direction === "forward") {
-    acc.position += cur.steps;
-  }
+function update_ones(acc: number[], line: BinaryString) {
+  line.forEach((value, index) => {
+    acc[index] += value;
+  });
   return acc;
-};
-const step_2_reducer = (acc: AimedLocation, cur: Command) => {
-  if (cur.direction === "down") {
-    acc.aim += cur.steps;
+}
+
+const raw_input = await readFile(process.argv[2], "utf8");
+const binary_lines = raw_input.split("\n").map(make_binary);
+const gamma_raw = binary_lines
+  .reduce(
+    update_ones,
+    binary_lines[0].map(() => 0)
+  )
+  .map((count) => {
+    if (count / binary_lines.length > 0.5) {
+      return 1;
+    } else {
+      return 0;
+    }
+  });
+const gamma = parseInt(gamma_raw.join(""), 2);
+const epsilon_raw = gamma_raw.map((x) => (x === 1 ? 0 : 1));
+const epsilon = parseInt(epsilon_raw.join(""), 2);
+console.log("Part 1:", gamma, epsilon, gamma * epsilon);
+
+function o2_generator_digit(
+  lines: readonly BinaryString[],
+  digit: number
+): BinaryNumber {
+  return lines.reduce((acc, line) => {
+    return acc + line[digit];
+  }, 0) /
+    lines.length >=
+    0.5
+    ? 1
+    : 0;
+}
+
+function co2_scrubber_digit(
+  lines: readonly BinaryString[],
+  digit: number
+): BinaryNumber {
+  return o2_generator_digit(lines, digit) === 1 ? 0 : 1;
+}
+
+function winnow_candidates(
+  candidates: readonly BinaryString[],
+  digit_finder: (lines: readonly BinaryString[], digit: number) => BinaryNumber
+): BinaryString {
+  let digit = 0;
+  while (candidates.length > 1) {
+    const filter_digit = digit_finder(candidates, digit);
+    candidates = candidates.filter((line) => line[digit] === filter_digit);
+    digit++;
   }
-  if (cur.direction === "up") {
-    acc.aim -= cur.steps;
-  }
-  if (cur.direction === "forward") {
-    acc.position += cur.steps;
-    acc.depth += acc.aim * cur.steps;
-  }
-  return acc;
-};
-const part_1 = parsed_input.reduce(step_1_reducer, { position: 0, depth: 0 });
-const part_2 = parsed_input.reduce(step_2_reducer, {
-  position: 0,
-  depth: 0,
-  aim: 0,
-});
-console.log("Part 1:", part_1.depth * part_1.position);
-console.log("Part 2:", part_2.depth * part_2.position);
+  return candidates[0];
+}
+
+const o2_rating = winnow_candidates([...binary_lines], o2_generator_digit);
+const co2_rating = winnow_candidates([...binary_lines], co2_scrubber_digit);
+const life_support_rating =
+  parseInt(o2_rating.join(""), 2) * parseInt(co2_rating.join(""), 2);
+console.log("Part 2:", life_support_rating);
